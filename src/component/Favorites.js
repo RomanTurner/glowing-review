075cc95d5e889db0245e2b2b46ac7bda0ffd7a2f
Roman Turner <rn.turner@live.com>
2021-03-29 14:18:31 -0700

    owls added

--- src/component/Favorites.js
+++ src/component/Favorites.js
@@ -6,7 +6,8 @@ import React from 'react'
 export default function Favorites() {
     return (
         <div>
-            Hi
+            Hi from Owly:
+             <img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/f192ec80151723.5cd963b84ffa1.jpg" alt="owl"/>
         </div>
     )
 }
